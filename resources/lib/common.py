4d23b735752a34eef086be53c6db8c9ea4175f2f
Maven85 <janis.semper@googlemail.com>
2023-01-20 07:55:00 +0100

    generation of a UUID for Android version 11 and higher, since...
    ... "Network.MacAddress" returns an empty string

--- resources/lib/common.py
+++ resources/lib/common.py
@@ -6,7 +6,7 @@ from kodi_six.utils import PY2, py2_encode
 from base64 import b64encode as base64_b64encode, b64decode as base64_b64decode
 from hashlib import md5 as hashlib_md5
 from time import sleep as time_sleep
-from uuid import UUID as uuid_UUID
+from uuid import UUID as uuid_UUID, uuid1 as uuid_uuid1
 
 import xbmc
 import xbmcgui
@@ -86,19 +86,38 @@ class Common:
     def uniq_id(self):
         device_id = ''
         mac_addr = xbmc.getInfoLabel('Network.MacAddress')
-
-        # hack response busy
-        i = 0
-        while not py2_encode(':') in mac_addr and i < 3:
-            i += 1
-            time_sleep(1)
-            mac_addr = xbmc.getInfoLabel('Network.MacAddress')
         if py2_encode(':') in mac_addr:
             device_id = str(uuid_UUID(hashlib_md5(mac_addr.encode('utf-8')).hexdigest()))
-        else:
+        elif xbmc.getCondVisibility('System.Platform.Android'):
+            device_id = str(uuid_UUID(hashlib_md5(self.get_android_uuid().encode('utf-8')).hexdigest()))
+
+        if device_id == '':
             self.log('[{0}] error: failed to get device id ({1})'.format(self.addon_id, str(mac_addr)))
             self.dialog_ok('Geräte-Id konnte nicht ermittelt werden.')
+
         return device_id
+
+
+    def get_android_uuid(self):
+        from subprocess import subprocess_PIPE, subprocess_Popen
+        from re import sub as re_sub
+        values = ''
+        try:
+            # Due to the new android security we cannot get any type of serials
+            sys_prop = ['ro.product.board', 'ro.product.brand', 'ro.product.device', 'ro.product.locale'
+                        'ro.product.manufacturer', 'ro.product.model', 'ro.product.platform',
+                        'persist.sys.timezone', 'persist.sys.locale', 'net.hostname']
+            # Warning net.hostname property starting from android 10 is deprecated return empty
+            with subprocess_Popen(['/system/bin/getprop'], stdout=subprocess_PIPE) as proc:
+                output_data = proc.communicate()[0].decode('utf-8')
+            list_values = output_data.splitlines()
+            for value in list_values:
+                value_splitted = re_sub(r'\[|\]|\s', '', value).split(':')
+                if value_splitted[0] in sys_prop:
+                    values += value_splitted[1]
+        except Exception:
+            pass
+        return values
 
 
     def set_videoinfo(self, listitem, infolabels):
